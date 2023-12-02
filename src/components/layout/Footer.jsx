04cd4e23ff3273702ff49c793b6581d6b19d1e15
Aidan Boling <aidanboling@gmail.com>
2023-12-02 15:42:58 -0800

    Add copyright text to footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,13 @@
+import content from '@/data/about.json';
+
 export default function Footer() {
-    return <div className="w-screen h-[140px] bg-gray-950/90 mt-16"></div>;
+    return (
+        <footer className="w-screen h-[140px] bg-gray-950/90 mt-16">
+            <div className="mx-auto max-w-xs h-full py-8 flex justify-center">
+                <small className="self-end">
+                    &copy; {content.name} {new Date().getFullYear()}
+                </small>
+            </div>
+        </footer>
+    );
 }
