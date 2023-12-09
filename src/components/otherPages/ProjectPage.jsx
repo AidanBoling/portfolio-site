b0f6f90ca2a6fe5397573684a7e6621fa8a1b504
Aidan Boling <aidanboling@gmail.com>
2023-12-09 14:16:14 -0800

    Update md parser image component

--- src/components/otherPages/ProjectPage.jsx
+++ src/components/otherPages/ProjectPage.jsx
@@ -1,28 +1,59 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
 import OuterContainer from '../layout/OuterContainer';
 
 export default function ProjectPage({ project }) {
-    const imgComponent = {
-        img({ node, alt, src, ...rest }) {
-            return (
-                <div className="mdImageContainer" {...rest}>
-                    <Image
-                        src={src}
-                        alt={alt}
-                        width="600"
-                        height="300"
-                        style={{ objectFit: 'contain' }}
-                    />
-                </div>
-            );
+    const components = {
+        p: ({ node, children }) => {
+            if (node.children[0].tagName === 'img') {
+                const { alt, src, ...rest } = node.children[0].properties;
+                return (
+                    <div
+                        className="not-prose w-full max-w-[950px] aspect-[70/45] max-h-[400px] m-0 relative"
+                        {...rest}>
+                        <Image
+                            src={src}
+                            alt={alt}
+                            fill
+                            sizes="(max-width: 600px) 95vw, (max-width: 1024px) 560px, 950px"
+                            style={{
+                                maxWidth: '950px',
+                                maxHeight: '400px',
+                                objectFit: 'contain',
+                                // position: 'relative !important',
+                            }}
+                        />
+                    </div>
+                );
+            }
+            // Return default child if it's not an image
+            return <p>{children}</p>;
         },
     };
+
+    // const imgComponent = {
+    //     img({ node, alt, src, ...rest }) {
+    //         return (
+    //             <div className="mdImageContainer outer">
+    //                 <div className="mdImageContainer inner" {...rest}>
+    //                     <Image
+    //                         src={src}
+    //                         alt={alt}
+    //                         fill
+    //                         sizes="950px"
+    //                         style={{ objectFit: 'contain' }}
+    //                     />
+    //                 </div>
+    //             </div>
+    //         );
+    //     },
+    // };
 
     return (
         <div className="pt-[75px] xxs:pt-[100px] xs:pt-[15vh] lg:pt-[18vh] pb-24 page-min-h relative">
             <OuterContainer>
                 <div className="w-full min-h-min content-bg content-max-size-x p-[5vw] xs:p-8 sm:p-12 rounded-sm ab-prose">
-                    <ReactMarkdown components={{ imgComponent }}>
+                    <ReactMarkdown components={components}>
                         {project.content}
                     </ReactMarkdown>
                 </div>
